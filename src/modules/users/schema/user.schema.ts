98fca2d9b9e09bbfb4763c57415a79c5aeddb4b1
Abdelrahman <136171089+obad7@users.noreply.github.com>
2025-10-17 01:45:43 +0300

    Update user schema & User types

--- src/modules/users/schema/user.schema.ts
+++ src/modules/users/schema/user.schema.ts
@@ -1,18 +1,75 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { MongooseModule, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, HydratedDocument } from 'mongoose';
+import { UserRoles, GenderType } from 'src/modules/users/schema/user.types';
 
 export type UserDocument = User & Document;
 
+@Schema({
+  timestamps: true,
+})
 @Schema()
 export class User {
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    trim: true,
+    lowercase: true,
+  })
   name: string;
 
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    index: { name: 'username_index' },
+  })
+  username: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    index: { name: 'email_index' },
+  })
   email: string;
 
-  @Prop({ default: Date.now })
-  createdAt: Date;
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRoles,
+    default: UserRoles.USER,
+  })
+  role: string;
+
+  @Prop({ type: String, enum: GenderType, default: GenderType.MALE })
+  gender: string;
+
+  @Prop({ type: String, unique: true })
+  phoneNumber: string;
+
+  @Prop({ type: Boolean, default: false })
+  emailVerified: boolean;
+
+  @Prop({ type: Date })
+  DOB: Date;
+
+  @Prop({ type: Boolean, default: false })
+  isDeleted: boolean;
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
+const userSchema = SchemaFactory.createForClass(User);
+
+// Registers the User schema within NestJS for dependency injection (so it can be used inside services/repositories)
+export const UserModel = MongooseModule.forFeature([
+  { name: User.name, schema: userSchema },
+]);
+
+// Defines a TypeScript type for a fully-hydrated Mongoose User document (with Mongoose methods and properties)
+export type UserType = HydratedDocument<User>;
