05174f4f64090221acd63c712b186e2e136bca91
Abdelrahman <136171089+obad7@users.noreply.github.com>
2025-10-17 15:23:23 +0300

    setup auth Service & Controller

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { SignUpDTO } from './DTOs/auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor() {}
+
+  async signUpService(body: SignUpDTO) {
+    return body;
+  }
 }
