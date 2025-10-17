22e5a63dc1ca0a5aa7e52541c76ffe745880051b
Abdelrahman <136171089+obad7@users.noreply.github.com>
2025-10-18 00:52:58 +0300

    Added security - encryption & hashing

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,12 +3,14 @@ import { Body, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { SignUpDTO } from './DTOs/auth.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
+  @ApiProperty({ type: SignUpDTO })
   async signup(
     @Body(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
     body: SignUpDTO,
