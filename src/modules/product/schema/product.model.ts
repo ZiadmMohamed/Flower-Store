5ba74575ff0e087ed75988c1bb64b725fb9c6670
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2025-10-18 01:24:20 +0300

    update product schema && product Repo

--- src/modules/product/schema/product.model.ts
+++ src/modules/product/schema/product.model.ts
@@ -7,7 +7,6 @@ import {
   Schema,
   SchemaFactory,
 } from '@nestjs/mongoose';
-import slugify from 'slugify';
 @Schema({ timestamps: true })
 export class Product implements Iproduct {
   @Prop({ required: true, min: 4 })
@@ -31,29 +30,13 @@ export class Product implements Iproduct {
   image: Iimage;
   @Prop({ type: String, enum: productStatus, default: productStatus.INSTOCK })
   status: productStatus;
-  @Prop({
-    required: false,
-    default: function (this: Product) {
-      return slugify(this.productName, { trim: true });
-    },
-  })
-  slug: string;
+ 
 }
 export type productDocument = HydratedDocument<Product>;
 export const productSchema = SchemaFactory.createForClass(Product);
-export const productModel = MongooseModule.forFeatureAsync([
+export const productModel = MongooseModule.forFeature([
   {
-    name: Product.name,
-    useFactory() {
-      productSchema.pre('updateOne', function (next) {
-        let update = this.getUpdate();
-        if (update['productName']) {
-          update['slug'] = slugify(update['productName'], { trim: true });
-          this.setUpdate(update);
-          next();
-        }
-      });
-      return productSchema;
-    },
-  },
+    name: Product.name, schema:productSchema
+    }
+  
 ]);
