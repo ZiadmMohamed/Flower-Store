1fd7c3ffd5e674da4513cb25f680a3cf74ea81b7
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2025-10-18 01:08:58 +0300

    update product schema && product Repo

--- src/modules/product/product.interface.ts
+++ src/modules/product/product.interface.ts
@@ -1,19 +1,19 @@
-import { Types } from "mongoose";
-export interface Iimage{
-    secure_url:string;
-    public_id:string
+import { Types } from 'mongoose';
+export interface Iimage {
+  secure_url: string;
+  public_id: string;
 }
-export enum productStatus{
-INSTOCK="INSTOCK",
-OUTSTOCK="OUTSTOCK",
+export enum productStatus {
+  INSTOCK = 'INSTOCK',
+  OUTSTOCK = 'OUTSTOCK',
 }
-export interface Iproduct{
-    productName:string;
-    description:string;
-    createdBy:Types.ObjectId;
-    stock:number;
-    originalPrice:number;
-    finalPrice:number;
-    image:Iimage
-    status:productStatus
+export interface Iproduct {
+  productName: string;
+  description: string;
+  createdBy: Types.ObjectId;
+  stock: number;
+  originalPrice: number;
+  finalPrice: number;
+  image: Iimage;
+  status: productStatus;
 }
