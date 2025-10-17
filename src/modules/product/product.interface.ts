d192e1a3db7e96eeab6d4ac095cb0dfbb0761d9a
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2025-10-18 00:46:22 +0300

    update product schema && product Repo

---
+++ src/modules/product/product.interface.ts
@@ -0,0 +1,19 @@
+import { Types } from "mongoose";
+export interface Iimage{
+    secure_url:string;
+    public_id:string
+}
+export enum productStatus{
+INSTOCK="INSTOCK",
+OUTSTOCK="OUTSTOCK",
+}
+export interface Iproduct{
+    productName:string;
+    description:string;
+    createdBy:Types.ObjectId;
+    stock:number;
+    originalPrice:number;
+    finalPrice:number;
+    image:Iimage
+    status:productStatus
+}
---
+++ src/modules/product/schema/product.model.ts
@@ -0,0 +1,47 @@
+import { HydratedDocument, Types } from "mongoose";
+import { Iimage, Iproduct, productStatus } from "../product.interface";
+import { MongooseModule, Prop, raw, Schema, SchemaFactory } from "@nestjs/mongoose";
+import slugify from "slugify";
+@Schema({timestamps:true})
+export class Product implements Iproduct{
+    @Prop({ required:true,min:4 })
+    productName:string;
+        @Prop({ required:true,min:4 })
+
+    description:string;
+        @Prop({type:Types.ObjectId ,required:true })
+
+    createdBy:Types.ObjectId;
+    @Prop({type:Number,required:true})
+    stock:number;
+        @Prop({type:Number,required:true})
+
+    originalPrice:number;
+        @Prop({type:Number,required:true})
+
+    finalPrice:number;
+        @Prop(raw({secure_url:{type:String,required:true},publick_id:{type:String,required:true}}))
+
+    image:Iimage
+    @Prop({type:String,enum:productStatus,default:productStatus.INSTOCK})
+    status:productStatus
+    @Prop({ required:false,default:function(this:Product){
+    return slugify(this.productName,{trim:true})
+} })
+slug: string;
+    
+}
+export type productDocument=HydratedDocument<Product>
+export const productSchema=SchemaFactory.createForClass(Product)
+export const productModel=MongooseModule.forFeatureAsync([{name:Product.name,useFactory(){
+    productSchema.pre("updateOne",function(next){
+        let update=this.getUpdate()
+        if (update["productName"]){
+            update['slug']=slugify(update['productName'],{trim:true})
+                      this.setUpdate(update)
+                      next()
+        }
+
+    })
+    return productSchema
+}}])
