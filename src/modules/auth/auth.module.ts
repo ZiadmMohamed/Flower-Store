b7ab7dbbd29760283ca5a7794b77a9b678e54a2c
Abdelrahman <136171089+obad7@users.noreply.github.com>
2025-10-17 15:52:52 +0300

    signUp Users to DB

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModel } from '../users/schema/user.schema';
+import { UserRepo } from 'src/common/Repositories/user.repo';
 
 @Module({
+  imports: [UserModel],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UserRepo],
 })
 export class AuthModule {}
