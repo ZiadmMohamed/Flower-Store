6ffde9b8f68ae729bbe3c24cc08d07de7c65f6cd
Abdelrahman <136171089+obad7@users.noreply.github.com>
2025-10-18 15:39:08 +0300

    Setup auth & role guards

---
+++ src/common/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+  NotFoundException,
+} from '@nestjs/common';
+import { UserRepo } from 'src/common/Repositories/user.repo';
+import { TokenService } from './../Services/token.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly tokenService: TokenService,
+    private readonly userRepo: UserRepo,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const { authorization } = request.headers;
+    if (!authorization)
+      throw new UnauthorizedException('Authorization header is required');
+
+    const data = this.tokenService.verifyToken(authorization, {
+      secret: process.env.JWT_SECRET,
+    });
+
+    const user = await this.userRepo.findByEmail(data.email);
+    if (!user) throw new NotFoundException('User not found');
+
+    request.authUser = user;
+    return true;
+  }
+}
