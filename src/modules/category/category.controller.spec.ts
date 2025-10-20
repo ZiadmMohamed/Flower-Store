bb4745093df6f86153e00fee32500ce57a680377
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2025-10-20 21:06:53 +0300

    edit eslint

--- src/modules/category/category.controller.spec.ts
+++ src/modules/category/category.controller.spec.ts
@@ -1,14 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { CategoryRepo } from '../Repositories/category.repo';
 
 describe('CategoryController', () => {
   let controller: CategoryController;
-
+const mockCategoryRepo = {
+  findCategoryById: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  updateOne: jest.fn(),
+  // ... mock all other methods used by CategoryService
+};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoryController],
-      providers: [CategoryService],
+      providers: [CategoryService,{provide:CategoryRepo,useValue:mockCategoryRepo}],
     }).compile();
 
     controller = module.get<CategoryController>(CategoryController);
--- src/modules/category/category.service.spec.ts
+++ src/modules/category/category.service.spec.ts
@@ -1,17 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoryService } from './category.service';
+import { CategoryRepo } from '../Repositories/category.repo';
 
 describe('CategoryService', () => {
   let service: CategoryService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
-    }).compile();
+const mockCategoryRepo = {
+  findCategoryById: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  updateOne: jest.fn(),
+  // ... mock all other methods used by CategoryService
+};
 
-    service = module.get<CategoryService>(CategoryService);
-  });
-
+beforeEach(async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [
+      CategoryService,
+      {
+        provide: CategoryRepo, // <-- The dependency class
+        useValue: mockCategoryRepo, // <-- The mock object
+      },
+    ],
+  }).compile();})
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
