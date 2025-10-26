5e5b19cc2676fa8cc6c8487cf95a6f0fb708a51c
Abdelrahman <136171089+obad7@users.noreply.github.com>
2025-10-26 18:14:49 +0300

    created update Profile endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,19 @@
 import { UsersService } from './users.service';
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { Response } from 'express';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { RoleGuard } from 'src/common/guards/roles.guard';
+import { UpdateUserDTO } from './DTOs/users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -17,6 +26,20 @@ export class UsersController {
   async viewProfileService(@Req() req: Request, @Res() res: Response) {
     const authuser = req['authUser'];
     const results = await this.UsersService.viewProfile(authuser);
+    return res.status(200).json({ results });
+  }
+
+  @Post('update-profile')
+  @ApiBearerAuth()
+  @Roles(['user', 'admin'])
+  @UseGuards(AuthGuard, RoleGuard)
+  async updateProfileService(
+    @Body() body: UpdateUserDTO,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const authuser = req['authUser'];
+    const results = await this.UsersService.updateProfile(authuser, body);
     return res.status(200).json({ results });
   }
 }
