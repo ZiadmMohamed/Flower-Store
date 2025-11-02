6712d4f9f1022524bee9fc75caa90cca88b265fd
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2025-11-02 10:26:12 +0200

    swagger edit

--- src/modules/product/DTO/update.product.DTO.ts
+++ src/modules/product/DTO/update.product.DTO.ts
@@ -4,7 +4,6 @@ import { Types } from 'mongoose';
 import { IsMongoId } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-<<<<<<< HEAD
 export class updateProductDTO extends PartialType(CreatProductDTO) {
   @ApiProperty({
     description: 'The unique MongoDB ObjectId of the category.',
@@ -12,9 +11,6 @@ export class updateProductDTO extends PartialType(CreatProductDTO) {
     type: String,
     required: true,
   })
-=======
-export class UpdateProductDTO extends PartialType(CreatProductDTO) {
->>>>>>> c628750aed0141e31dd992d0cad5a5924d5efbc2
   @IsMongoId()
   categoryId: Types.ObjectId;
 }
